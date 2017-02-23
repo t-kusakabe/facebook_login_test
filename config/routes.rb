24d1cd0979835543013ac528c74a4a95acf450ea
t-kusakabe <ttmac0704@gmail.com>
2017-02-23 23:01:22 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/:provider/callback',    to: 'users#create',       as: :auth_callback
+  get '/auth/failure',               to: 'users#auth_failure', as: :auth_failure
+
+end
